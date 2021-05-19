1efd61d4ffca863367d355f00afc06e42d3fd8c9
bosciu <bosciu@icloud.com>
2021-05-19 13:58:57 +0200

    Aggiunto funzionamento input

--- js/script.js
+++ js/script.js
@@ -6,22 +6,32 @@ const app = new Vue({
                 completed: false
             },
             {
-                toDoTitle: "Cosa da fare 2",
+                toDoTitle: "Cosa da fare più lunga 2",
                 completed: false
             },
             {
-                toDoTitle: "Cosa da fare 3",
+                toDoTitle: "Cosa da fare molto più lunga 3",
                 completed: false
             },
             {
                 toDoTitle: "Cosa da fare 4",
                 completed: false
             }
-        ]
+        ],
+        toDoString: ""
     },
     methods: {
         removeItem: function(removedIndex) {
             this.toDoList.splice(removedIndex, 1);
+        },
+        submit: function() {
+            if (this.toDoString.trim().length > 0) {
+                this.toDoList.push({
+                    toDoTitle: this.toDoString,
+                    completed: false
+                });
+                this.toDoString = "";
+            }
         }
     }
 });
