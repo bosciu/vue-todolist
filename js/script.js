41f3076051d705dcb0aa73bae9c0c52f596ba63a
bosciu <bosciu@icloud.com>
2021-05-19 13:38:40 +0200

    Aggiunta funzione per svuotare lista

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 const app = new Vue({
     el: "#root",
     data: {
-        prova: 0
+        toDoList: [{
+                toDoTitle: "Cosa da fare 1",
+                completed: false
+            },
+            {
+                toDoTitle: "Cosa da fare 2",
+                completed: false
+            },
+            {
+                toDoTitle: "Cosa da fare 3",
+                completed: false
+            },
+            {
+                toDoTitle: "Cosa da fare 4",
+                completed: false
+            }
+        ]
+    },
+    methods: {
+        removeItem: function(removedIndex) {
+            this.toDoList.splice(removedIndex, 1);
+        }
     }
 });
